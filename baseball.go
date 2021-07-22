1fc899c73ce6342ff076c94323bfc7fd5a9ce987
sang5c <skah321@gmail.com>
2021-07-22 15:38:42 +0900

    refactor: change magic number to const

--- baseball.go
+++ baseball.go
@@ -9,15 +9,30 @@ type Game struct {
 	Quiz string
 }
 
+const (
+	NumberSeparator = ""
+	MinNumber       = "1"
+	MaxNumber       = "9"
+	NumberLength    = 3
+)
+
 func NewGame(s string) (*Game, error) {
-	numbers := strings.Split(s, "")
-	if len(numbers) != 3 {
+	numbers := strings.Split(s, NumberSeparator)
+	if invalidLength(numbers) {
 		return nil, fmt.Errorf("invalid length: %s", numbers)
 	}
 	for _, v := range numbers {
-		if v < "1" || v > "9" {
+		if numberOutOfRange(v) {
 			return nil, fmt.Errorf("invalid numbers: %s", numbers)
 		}
 	}
 	return &Game{s}, nil
+}
+
+func invalidLength(numbers []string) bool {
+	return len(numbers) != NumberLength
+}
+
+func numberOutOfRange(v string) bool {
+	return v < MinNumber || v > MaxNumber
 }
