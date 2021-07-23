255bab30f78f120344be02116f7e78f87d48dba7
sang5c <skah321@gmail.com>
2021-07-23 15:03:33 +0900

    move test code

--- baseball_test.go
+++ baseball_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"baseball/score"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -30,4 +31,23 @@ func assertError(t *testing.T, s string) {
 	game, err := NewGame(s)
 	assert.Nil(t, game)
 	assert.Error(t, err)
+}
+
+var (
+	game, _ = NewGame("123")
+)
+
+func TestNothing(t *testing.T) {
+	assertEqual(t, "456", 0, 0)
+}
+
+func TestStrike(t *testing.T) {
+	assertEqual(t, "123", 3, 0)
+	assertEqual(t, "143", 2, 0)
+	assertEqual(t, "425", 1, 0)
+}
+
+func assertEqual(t *testing.T, input string, strike int, ball int) {
+	result := game.compare(input)
+	assert.Equal(t, score.New(strike, ball), result)
 }
